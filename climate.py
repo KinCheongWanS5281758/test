82256490f2540908fe0c5288097aa3b7536d0a5e
KinCheongWanS5281758 <kincheong.wan@griffithuni.edu.au>
2022-09-15 13:49:00 +1000

    update climate.py

--- climate.py
+++ climate.py
@@ -1,11 +1,22 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor()
+cursor.execute("SELECT * from ClimateData")
+climatedata = cursor.fetchall()
 years = []
 co2 = []
 temp = []
 
+for row in climatedata:
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
 
+plt.plot()
 plt.subplot(2, 1, 1)
+# 2 rows, 1 column, 1st plot
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
 plt.ylabel("[CO2]") 
